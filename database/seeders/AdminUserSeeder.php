083046ead9a6a3036ad8bb12bc77688efe818dcc
Edo <editestar@gmail.com>
2026-05-17 16:16:30 +0200

    chore: aggiorna email utenti di test in AdminUserSeeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -13,7 +13,7 @@ class AdminUserSeeder extends Seeder
         // Admin
         User::create([
             'name' => 'Admin',
-            'email' => 'admin@test.com',
+            'email' => 'admin@admin.admin',
             'password' => Hash::make('password'),
             'role' => User::ROLE_ADMIN,
 //             'permissions' => [], // ROLE_ADMIN bypassa tutto
@@ -22,7 +22,7 @@ class AdminUserSeeder extends Seeder
         // Editor
         User::create([
             'name' => 'Editor',
-            'email' => 'editor@test.com',
+            'email' => 'editor@editor.editor',
             'password' => Hash::make('password'),
             'role' => User::ROLE_EDITOR,
             'permissions' => [
@@ -34,7 +34,7 @@ class AdminUserSeeder extends Seeder
         // Viewer
         User::create([
             'name' => 'Viewer',
-            'email' => 'viewer@test.com',
+            'email' => 'viewer@viewer.viewer',
             'password' => Hash::make('password'),
             'role' => User::ROLE_VIEWER,
             'permissions' => [],
